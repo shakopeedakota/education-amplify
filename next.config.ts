3450f80c5cd07ef5fa1364ca4f964150b83cd562
Connor Chamberlain <connor.chamb@gmail.com>
2025-06-25 10:57:13 -0500

    More updates

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      react: require.resolve('react'),
-      'react-dom': require.resolve('react-dom'),
-      canvas: false,
-    };
-    return config;
-  },
 };
 
 export default nextConfig;
